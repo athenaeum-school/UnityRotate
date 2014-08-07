d36a5a8b871cf42f92ae828535dce481c02380a8
Komatsu <school@athenaeum-society.com>
2014-08-07 13:33:37 +0900

    Update RotatorScript.cs
    bug fix1

--- RotatorScript.cs
+++ RotatorScript.cs
@@ -4,30 +4,17 @@ using System.Collections;
 public class RotateScript : MonoBehaviour
 {
 	GameObject cubeOne;
-	//Rotator rotateOne;
 	
 	void Start ()
 	{
 		cubeOne = GameObject.Find("Cube");
-		Debug.Log(cubeOne);
-		//rotateOne = GameObject.Find("Cube").GetComponent<Rotator>();
-		Debug.Log("Start");
 	}
 	
 	void update()
 	{
-		Debug.Log("Update");
 		if(Input.GetKey(KeyCode.Return))
 		{
-			//rotateOne.RotLeft();
 			cubeOne.GetComponent<Rotator>().RotLeft();
-			Debug.Log("LeftArrow");
-		}
-		if(Input.GetKey(KeyCode.RightArrow))
-		{
-			//rotateOne.RotRight();
-			cubeOne.GetComponent<Rotator>().RotRight();
-			Debug.Log("RightArrow");
 		}
 	}
 }
